953f98ec24df0a0c29ee74b7ee6a3446da390a7e
Brenon <brenonparry@gmail.com>
2022-01-06 22:04:34 +0000

    update getUserWithEmail, getUserWithId & addUser functions to return SQL queried promise

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,16 +18,21 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  // let user;
+  // for (const userId in users) {
+  //   user = users[userId];
+  //   if (user.email.toLowerCase() === email.toLowerCase()) {
+  //     break;
+  //   } else {
+  //     user = null;
+  //   }
+  // }
+  // return Promise.resolve(user);
+
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1;`, [email] || null)
+    .then((result) => result.rows[0])
+    .catch((err) => err)
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +42,10 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1;`, [id] || null)
+    .then((result) => result.rows[0])
+    .catch((err) => err)
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +56,18 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password)
+       VALUES ($1, $2, $3)
+       RETURNING*;`, [user.name, user.email, user.password])
+    .then((result) => {return result.rows[0]})
+    .catch((err) => err)
 }
 exports.addUser = addUser;
 
